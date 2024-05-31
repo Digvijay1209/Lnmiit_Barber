cbe9341c2c0d8e494561ef1963911505fd0311d2
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-05-31 22:32:12 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault()
-    axios.post('barberbackend-digvijay1209s-projects.vercel.app/Login', { email, password })
+    axios.post('backend-plum-alpha-76.vercel.app/Login', { email, password })
       .then(result => {
         console.log(result.data);
         
