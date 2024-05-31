72bc94379c99817a9b311ff0a0424378de278cec
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-05-31 21:56:39 +0530

    Update Dashboard.jsx

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -13,7 +13,7 @@ function Dashboard({ disabledButtons, handleAvailableClick, handleNotAvailableCl
     axios.defaults.withCredentials = true;
 
     useEffect(() => {
-        axios.get('http://localhost:3001/Dashboard')
+        axios.get('https://barberbackend-n1ypnfxhk-digvijay1209s-projects.vercel.app/Dashboard')
             .then(res => {
                 console.log(res.data);
                 if (res.data === "Success") {
@@ -28,7 +28,7 @@ function Dashboard({ disabledButtons, handleAvailableClick, handleNotAvailableCl
     useEffect(() => {
      
            
-            axios.get('http://localhost:3001/Dashboard_1')
+            axios.get('https://barberbackend-n1ypnfxhk-digvijay1209s-projects.vercel.app/Dashboard_1')
             .then(response => {
                 console.log(response.data);
                 setFet(response.data);
@@ -41,7 +41,7 @@ function Dashboard({ disabledButtons, handleAvailableClick, handleNotAvailableCl
     const handleClick=(timing)=>{
         
        
-        axios.post('http://localhost:3001/dashboard', {timing,status})
+        axios.post('https://barberbackend-n1ypnfxhk-digvijay1209s-projects.vercel.app/dashboard', {timing,status})
         .then(result => {console.log(result) 
             window.location.reload();
         }).catch(err => console.log(err))
