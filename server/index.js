5d3789e66687d377f31fffba11575db95b74e198
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-05-31 22:35:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: ["barberfrontend-digvijay1209s-projects.vercel.app"],
+  origin: ["frontend-sable-tau-61.vercel.app"],
   methods: ["GET", "POST"],
   credentials: true
 }))
