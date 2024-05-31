24b9ec6de5d3114f05e99faf2463245475a10abf
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-05-31 21:58:23 +0530

    Update Signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/register', {name, email, password})
+        axios.post('https://barberbackend-n1ypnfxhk-digvijay1209s-projects.vercel.app/register', {name, email, password})
         .then(result => {console.log(result) 
             navigate('/Login')
         }).catch(err => console.log(err))
