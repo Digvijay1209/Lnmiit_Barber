7d964a4794611b53cb7b9e01fe33734d08fd3766
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-06-02 22:07:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,10 +10,33 @@ const cookieParser = require('cookie-parser')
 const app = express()
 app.use(express.json())
 
+const express = require("express");
+const app = express();
+// Set middleware of CORS 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "frontend-sable-tau-61.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+ 
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use(cors({
-//  origin: ["frontend-sable-tau-61.vercel.app"],
-  origin: ["*"],
+origin: ["frontend-sable-tau-61.vercel.app"],
+  
   methods: ["GET", "POST"],
   credentials: true
 }))
